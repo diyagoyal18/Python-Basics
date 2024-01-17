e572c3a6ce8d831e79bb3ed7a986e950ebf18705
Diya Goyal <dgoyal6_be22@thapar.edu>
2024-01-17 21:13:55 +0530

    variables

--- intro.py
+++ intro.py
@@ -1,1 +1,10 @@
 print("hello")
+#comment
+
+
+# python commant line interface
+# REPL-> read evauate print loop
+
+#variables are containers used to store data
+#variables donot be declared with any particular type
+#it is dynamically typed
---
+++ variables.py
@@ -0,0 +1,28 @@
+name ="diya"
+
+roll_number=21
+print(type(name))
+percentage=96.3
+
+is_student= True
+
+print(name)
+print(roll_number, percentage, is_student)
+
+percentage=54.2
+
+print(roll_number, percentage, is_student)
+
+print("my name is" +name+ "and my roll no is", roll_number)
+# + symbol is used when we have same data type
+# and , is used when we have different data types
+
+
+
+#print expressions
+print("my score is changed to", percentage-1.0)
+
+
+
+#printing with seperator
+print(name, roll_number, percentage, is_student, sep="-")
