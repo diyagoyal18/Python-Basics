5d576b269fd94299d7845e36ee9630ecbbf1cfc2
Diya Goyal <dgoyal6_be22@thapar.edu>
2024-01-17 21:57:33 +0530

    data types

---
+++ basic introduction/dataTypes.py
@@ -0,0 +1,39 @@
+# data types 
+
+#     numeric data types
+#         int
+#         float
+#         complex
+
+#     boolean data types
+#         True
+#         False
+
+#     sequence data types
+#         String
+#         list ->stored in square brackets, modifiable data hota h, it can be sequence of integers or strings, unique values
+#         tuple-> stored in round brackets, duplicate values stored, not modifiable
+
+#     dictionary data types -> key value pairs
+
+#     set data type-> unordered collection of unique values
+
+#     none data type-> null value stored
+
+
+# ASCII VALUES
+
+# A-Z     65-90
+# a-z     97-122
+# 0-9     48-57
+# space   32
+
+char = 'a'
+print(ord(char)) 
+
+#ord function directly prints the ascii value of a single character
+
+#chr function is opposite ord function
+
+ascii = 68
+print(chr(ascii))
