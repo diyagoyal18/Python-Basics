0eb5f7814301019ec1e874a77307ea830ffa52c0
Diya Goyal <dgoyal6_be22@thapar.edu>
2024-01-21 10:57:33 +0530

    escape characters

---
+++ basic introduction/escapeCharacters.py
@@ -0,0 +1,9 @@
+ # \' ->single qoute
+ # \\ -> backslash
+ # /n -> new line
+ # \r -> carriage return
+ # \t -> tab
+ # \b -> backspace
+ # \f -> form feed
+ # \ooo -> octal value
+ # \xhh -> hex value
